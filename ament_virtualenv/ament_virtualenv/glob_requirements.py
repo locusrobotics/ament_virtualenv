590f556e6cc2bd6c04fcced1609599026207d191
James Prestwood <jprestwood@locusrobotics.com>
2026-03-31 10:19:09 -0700

    Fix non merge install (#13)
    * Fix non-merge install workspaces
    
    Ignore files may be sitting at the top level directory, which
    prevents the search from coming up with anything. In addition a
    shortcut was added to check for the package much faster than
    iterating the entire install tree.
    
    * Pass in source directory so files can be found
    
    * Adapt source dir to work with pure python packages
    
    * Install venv under a packages share directory
    
    The venv folder was getting placed at the root install folder which
    basically causes every package in the workspace to overwrite the venv
    of other packages, last package wins...
    
    * Fix venv location again, and generated requirements file
    
    * Remove unreliable/unneeded find_in_workspace code
    
    By passing the source directory as well as using the proper ament
    API to find the package prefix, searching the env vars with assumed
    paths isn't reliable or useful anymore.

--- ament_virtualenv/ament_virtualenv/glob_requirements.py
+++ ament_virtualenv/ament_virtualenv/glob_requirements.py
@@ -46,61 +46,9 @@ except ImportError:
 
 
 def find_in_workspaces(project, file, workspaces=[]):
-    # Add default workspace search paths
-    ament_paths = os.environ.get('AMENT_PREFIX_PATH')
-    if ament_paths is not None:
-        # AMENT_PREFIX_PATH points at <prefix>/install
-        ament_paths = ament_paths.split(os.pathsep)
-        for path in ament_paths:
-            if ((os.path.sep + 'install') in path or
-               (os.path.sep + 'install_isolated') in path):
-                workspaces.append(os.path.join(path, '..'))
-                workspaces.append(os.path.join(path, '..', '..', 'src'))
-                break
-    if len(workspaces) == 0:
-        # if AMENT_PREFIX_PATH wasn't set, we can fall back on
-        # CMAKE_PREFIX_PATH (should contain the same information)
-        cmake_paths = os.environ.get('CMAKE_PREFIX_PATH')
-        if cmake_paths is not None:
-            # CMAKE_PREFIX_PATH points at <prefix>/install or <prefix>/install_isolated
-            cmake_paths = cmake_paths.split(os.pathsep)
-            for path in cmake_paths:
-                if ((os.path.sep + 'install') in path or
-                   (os.path.sep + 'install_isolated') in path):
-                    workspaces.append(os.path.join(path, '..'))
-                    workspaces.append(os.path.join(path, '..', '..', 'src'))
-                    break
-    if len(workspaces) == 0:
-        # COLCON_PREFIX_PATH points to the `install/` directory,
-        # which is fine when ament_python is used as build tool
-        # (ament_python copies the files right away),
-        # but ament_cmake does not copy the files until after the
-        # build, which is too late. So for ament_cmake we also
-        # need to add the neighboring `src/` folder to the seach
-        # (eg.: `install/../src/`)
-        colcon_paths = os.environ.get('COLCON_PREFIX_PATH')
-        if colcon_paths is not None:
-            colcon_paths = colcon_paths.split(os.pathsep)
-            for path in colcon_paths:
-                if (os.path.sep + 'install') in path or (os.path.sep + 'install_isolated') in path:
-                    workspaces.append(path)
-                    workspaces.append(os.path.join(path, '..', 'src'))
-    if len(workspaces) == 0:
-        # If $CWD/src exists, append that
-        path = Path(os.getcwd()) / "src"
-        if path.exists():
-            workspaces.append(str(path))
-
-        # Also append the current directory, in case we're building from the
-        # package directory itself
-        workspaces.append(os.getcwd())
-
-    # Above, all paths required an "install/" or "src/" folder in order to qualify
-    # as a workspace. This only applies to local workspaces but does not take into
-    # account any installed bundles. We should prefer local workspaces, but we
-    # should also include the bundle path in case the workspace found above either
-    # doesn't contain the package in question or if its ignored. Since the first
-    # match is used we are safe to append the bundle path unconditionally.
+    # A source directory should be passed within 'workspaces' (i.e. a local workspace),
+    # but if not try and get the package path from ament. This will handle packages
+    # that aren't in the local workspace.
     try:
         path = get_package_share_directory(project)
         workspaces.append(path)
@@ -120,16 +68,22 @@ def find_in_workspaces(project, file, workspaces=[]):
     # <prefix>/install/<project>/../
     # The issue here is <project> dir may not actually exist so below when we walk the
     # directories it will ignore that folder since it doesn't exist. To fix this we
-    # need to resolve the paths so they point to valid directories, ignoring the di
+    # need to resolve the paths so they point to valid directories, ignoring the dir
     # of the packages we're building which may not exist.
     workspaces = [str(Path(path).resolve()) for path in workspaces]
 
-    # now search the workspaces
+    for workspace in (workspaces or []):
+        # Shortcut so we don't have to search the entire install tree
+        if os.path.exists(f"{workspace}/{project}/share/{project}/{file}"):
+            return f"{workspace}/{project}/share/{project}/{file}"
+
+    # Now search the workspaces. Since this may point to the distro root which
+    # will contain ignore files we need to ignore that directory i.e. d != workspace.
     for workspace in (workspaces or []):
         for d, dirs, files in os.walk(workspace, topdown=True, followlinks=True):
             if (('CATKIN_IGNORE' in files) or
                ('COLCON_IGNORE' in files) or
-               ('AMENT_IGNORE' in files)):
+               ('AMENT_IGNORE' in files)) and d != workspace:
                 del dirs[:]
                 continue
             dirname = os.path.basename(d)
@@ -143,13 +97,14 @@ def find_in_workspaces(project, file, workspaces=[]):
 AMENT_VIRTUALENV_TAGNAME = "pip_requirements"
 
 
-def parse_exported_requirements(package: Package) -> List[str]:
+def parse_exported_requirements(package: Package, source_dir: str) -> List[str]:
     requirements_list = []
     for export in package.exports:
         if export.tagname == AMENT_VIRTUALENV_TAGNAME:
             requirements_path = find_in_workspaces(
                 project=package.name,
-                file=export.content
+                file=export.content,
+                workspaces=[source_dir]
             )
             if not requirements_path:
                 print(
@@ -167,9 +122,12 @@ def parse_exported_requirements(package: Package) -> List[str]:
     return requirements_list
 
 
-def process_package(package_name, soft_fail=True):
+def process_package(package_name, source_dir=None, soft_fail=True):
     # type: (str) -> List[str], List[str]
     workspaces = []
+    if source_dir:
+        workspaces.append(source_dir)
+
     package_path = find_in_workspaces(
         project=package_name,
         file="package.xml",
@@ -185,10 +143,10 @@ def process_package(package_name, soft_fail=True):
     else:
         package = parse_package(package_path)
         dependencies = package.build_depends + package.test_depends
-        return parse_exported_requirements(package), dependencies
+        return parse_exported_requirements(package, source_dir), dependencies
 
 
-def glob_requirements(package_name, no_deps):
+def glob_requirements(package_name, source_dir, no_deps):
     # type: (str) -> int
     package_queue = Queue()
     package_queue.put(package_name)
@@ -201,7 +159,7 @@ def glob_requirements(package_name, no_deps):
         if queued_package not in processed_packages:
             processed_packages.add(queued_package)
             requirements, dependencies = process_package(
-                package_name=queued_package, soft_fail=(queued_package != package_name))
+                package_name=queued_package, source_dir=source_dir, soft_fail=(queued_package != package_name))
             requirements_list = requirements_list + requirements
 
             if not no_deps:
@@ -213,6 +171,7 @@ def glob_requirements(package_name, no_deps):
 def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser()
     parser.add_argument('--package-name', type=str, required=True)
+    parser.add_argument('--source-dir', type=str)
     parser.add_argument('--no-deps', action="store_true")
     args, unknown = parser.parse_known_args()
     print(glob_requirements(**vars(args)))
--- ament_virtualenv/ament_virtualenv/install.py
+++ ament_virtualenv/ament_virtualenv/install.py
@@ -27,6 +27,8 @@ import sys
 import subprocess
 import shutil
 import argparse
+
+from pathlib import Path
 from setuptools.command.install import install
 from setuptools import Distribution
 from typing import List, Dict
@@ -70,9 +72,10 @@ def install_venv(
         scripts_base: str,
         scripts: List[str] = [],
         python_version: str = '3',
-        use_system_packages: bool = True
+        use_system_packages: bool = True,
+        source_dir = None
 ):
-    venv_install_dir = os.path.join(install_base, 'venv')
+    venv_install_dir = os.path.join(install_base, 'share', package_name, 'venv')
     #
     # Build the virtual environment
     python = shutil.which("python3")
@@ -100,12 +103,12 @@ def install_venv(
         requirements_list = requirements_list.decode("utf-8").strip()
     else:
         # Use the module directly
-        requirements_list = glob_requirements(package_name=package_name, no_deps=False)
+        requirements_list = glob_requirements(package_name=package_name, source_dir=source_dir, no_deps=False)
     # ^ glob_requirements
     #
     # combine_requirements --requirements-list a/requirements.txt;b/requirements.txt
     #                      --output-file x/generated_requirements.txt
-    generated_requirements = os.path.join(install_base,
+    generated_requirements = os.path.join(install_base, 'share', package_name,
                                           package_name + '-generated_requirements.txt')
     if ament_virtualenv_import_failed:
         combine_requirements_py = find_program(name='combine_requirements.py',
@@ -233,10 +236,13 @@ class AmentVirtualenvInstall(install):
     def run(self):
         super().run()
 
+        source_dir = Path(self.distribution.script_name).resolve().parent
+
         install_venv(
             install_base=self.install_base,
             package_name=self.config_vars["dist_name"],
             scripts_base=self.install_scripts,
+            source_dir=str(source_dir),
             scripts=_get_console_scripts(self.distribution.entry_points),
             **_get_extra_arguments(self.distribution)
         )
